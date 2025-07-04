6649e8cd665a01df52508bcc5b0d515eca9d9865
Abhi Mansukhbhai Chakrani <156213847+Abhichakrani@users.noreply.github.com>
2025-07-04 09:20:30 -0400

    sprint 3 progess

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,7 +51,7 @@ export default function HomePage() {
 </h1>
 
         <h2 className="text-2xl sm:text-3xl font-bold mb-4 text-[#fdfaf5]">
-          Making Ontario Better, One Report at a Time
+          Making Ontario Better, One Report at a Time.
         </h2>
         <p className="text-md sm:text-lg text-gray-300 leading-relaxed">
           Our app empowers Ontario residents to report and track public issues like potholes,
@@ -140,10 +140,11 @@ export default function HomePage() {
         <h3 className="text-3xl font-bold text-center text-orange-500 mb-10 tracking-wide">
           Meet the Team
         </h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 justify-items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6 justify-items-center">
           {[
-            { name: "Vrunda Patel", gender: "female" },
             { name: "Sanskar Pardesi", gender: "male" },
+            { name: "Vrunda Patel", gender: "female" },
+            { name: "Yash Patel", gender: "male" },
             { name: "Nadi Lin", gender: "female" },
             { name: "Abhi Chakrani", gender: "male" },
           ].map((member, idx) => (
